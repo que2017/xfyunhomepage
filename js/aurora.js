8b158722e1118cb93ab5b35cc142982723b8cde6
zhangbx <bxzhang@iflytek.com>
2017-10-09 12:26:07 +0800

    极光特效

--- js/aurora.js
+++ js/aurora.js
@@ -1,43 +1,26 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="Generator" content="">
-    <meta name="Author" content="">
-    <meta name="Keywords" content="">
-    <meta name="Description" content="">
-    <title>Document</title>
-    <style type="text/css">
-        .aurora {
-            display: block;
-            margin: 100px auto;
-            box-shadow: 0 0 1px red;
-        }
-    </style>
-</head>
-<body>
-<canvas class="aurora" width="700" height="400"></canvas>
-<script type="text/javascript">
+(function () {
     var oAurora = document.getElementsByClassName('aurora')[0];
     var width = parseInt(oAurora.width);
     var height = parseInt(oAurora.height);
+    var overflow = 300;// 横向超出部分
     var cxt = oAurora.getContext('2d');
 
     function BezireCurve(color) {
         this.r = color.r;
         this.g = color.g;
         this.b = color.b;
-        this.speed = 20;
-        this.h = randomNum(50, 80);
-        this.params = [];
-        this.dParams = [];
+        this.distance = randomNum(30, 60); // 曲线横向最大宽度
+        this.speed = 20;// 曲线参数增量最大值
+        this.h = randomNum(30, 60); // 曲线扩展成面的宽度
+        this.params = []; //贝塞尔曲线参数
+        this.dParams = [];// 贝塞尔曲线参数增量
         var p0 = {}, p1 = {}, p2 = {}, p3 = {};
         // 随机产生贝塞尔函数的四个点，p1 p2在p0 p3构成的矩形内
-        p0.x = randomNum(0, width);
-        p0.y = randomNum(0, width);
-        p3.x = randomNum(0, height);
+        p0.x = randomNum(-overflow, width + overflow);
+        p0.y = randomNum(0, height);
+        p3.x = randomNum(Math.max(-overflow, p0.x - this.distance), Math.min(width + overflow, p0.x + this.distance));
         p3.y = randomNum(0, height);
-        this.dx = Math.abs(p0.x - p3.x);
+        this.dx = Math.abs(p0.x - p3.x); // 曲线定点的横向宽度
         p1.x = randomNum(Math.min(p0.x, p3.x), Math.max(p0.x, p3.x));
         p1.y = randomNum(0, height);
         p2.x = randomNum(Math.min(p0.x, p3.x), Math.max(p0.x, p3.x));
@@ -46,8 +29,10 @@
         // 随机产生四个点的增量
         for (var i = 0; i < 4; i++) {
             var dp = {};
-            dp.x = randomNum(-this.speed, this.speed);
-            dp.y = randomNum(-this.speed, this.speed);
+            var sign = Math.random(-1, 1) < 0 ? -1 : 1;
+            dp.x = randomNum(this.speed / 2, this.speed) * sign;
+            sign = Math.random(-1, 1) < 0 ? -1 : 1;
+            dp.y = randomNum(this.speed / 2, this.speed) * sign;
             this.dParams.push(dp);
         }
     }
@@ -79,22 +64,29 @@
                     this.params[i].y += this.dParams[i].y;
                 }
             }
-            if (this.params[0].x < 0 || this.params[0].x > width) {
+            this.dx = Math.abs(this.params[0].x - this.params[3].x);
+            if (this.params[0].x < -overflow || this.params[0].x > width + overflow) {
                 this.dParams[0].x = -this.dParams[0].x;
                 this.params[0].x += this.dParams[0].x;
+            } else if (this.dx > this.distance) {
+                this.dParams[0].x = -this.dParams[0].x;
             }
-            if (this.params[3].x < 0 || this.params[3].x > width) {
+            if (this.params[3].x < -overflow || this.params[3].x > width + overflow) {
                 this.dParams[3].x = -this.dParams[3].x;
                 this.params[3].x += this.dParams[3].x;
+            } else if (this.dx > this.distance) {
+                this.dParams[3].x = -this.dParams[3].x;
             }
             this.dx = Math.abs(this.params[0].x - this.params[3].x);
             var min = Math.min(this.params[0].x, this.params[3].x);
             var max = Math.max(this.params[0].x, this.params[3].x);
             if (this.params[1].x < min || this.params[1].x > max) {
+                this.params[1].x = this.params[1].x < min ? min : max;
                 this.dParams[1].x = -this.dParams[1].x;
                 this.params[1].x += this.dParams[1].x;
             }
             if (this.params[2].x < min || this.params[2].x > max) {
+                this.params[2].x = this.params[2].x < min ? min : max;
                 this.dParams[2].x = -this.dParams[2].x;
                 this.params[2].x += this.dParams[2].x;
             }
@@ -104,22 +96,23 @@
 //            console.log(this.params);
             var lastX = this.params[0].x;
             var lastY = this.params[0].y;
-            for (var i = 1; i < this.dx * 5; i++) {
-                var point = this.bezirePoint(0.2 * i / this.dx);
+            for (var i = 1; i < this.dx; i++) {
+                var point = this.bezirePoint(i / this.dx);
                 var x = parseInt(point.x);
                 var y = parseInt(point.y);
 //                var colorGrand = 20;
-                var r = this.r //+ randomNum(-colorGrand, colorGrand);
-                var g = this.g //+ randomNum(-colorGrand, colorGrand);
-                var b = this.b //+ randomNum(-colorGrand, colorGrand);
-                var a = 0.4 + randomNum(-1, 1) * 0.1;
+                var r = this.r;//+ randomNum(-colorGrand, colorGrand);
+                var g = this.g; //+ randomNum(-colorGrand, colorGrand);
+                var b = this.b; //+ randomNum(-colorGrand, colorGrand);
+                var a = 0.5 + randomNum(-1, 1) * 0.1;
                 var rgba0 = 'rgba(' + r + ',' + g + ',' + b + ', 0)';
                 var rgba1 = 'rgba(' + r + ',' + g + ',' + b + ', ' + a + ')';
                 cxt.save();
                 cxt.beginPath();
                 var color = cxt.createLinearGradient(x, y - this.h, x, y + this.h);
                 color.addColorStop(0, rgba0);
-                color.addColorStop(1, rgba1);
+                color.addColorStop(0.8, rgba1);
+                color.addColorStop(1, rgba0);
                 cxt.fillStyle = color;
                 cxt.moveTo(lastX, lastY - this.h);
                 cxt.lineTo(x, y - this.h);
@@ -136,19 +129,18 @@
         }
     };
     var aurora = [];
-    aurora.push(new BezireCurve({r: 80, g: 175, b: 100}));
-    aurora.push(new BezireCurve({r: 110, g: 25, b: 150}));
-    aurora.push(new BezireCurve({r: 80, g: 175, b: 100}));
+    for (var i = 0; i < 10; i++) {
+        aurora.push(new BezireCurve({r: 80, g: 175, b: 100}));
+        // aurora.push(new BezireCurve({r: 110, g: 25, b: 150}));
+    }
     setInterval(function () {
         cxt.clearRect(0, 0, width, height);
         for (var i = 0; i < aurora.length; i++) {
             aurora[i].draw();
         }
-    }, 10);
+    }, 100);
 
     function randomNum(min, max) {
         return parseInt(Math.random() * (max - min) + min);
     }
-</script>
-</body>
-</html>
+})();
